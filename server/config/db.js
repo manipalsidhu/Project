a698cef1ef4420bfe36bbda113c71ac44661b06f
manipalsidhu <manipals@yahoo.com>
2021-11-14 17:18:06 -0500

    Manipal Sidhu

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+module.exports.URI = "mongodb+srv://TLB2310:tlb2310@cluster0.ldslt.mongodb.net/TLB?retryWrites=true&w=majority";
+//module.exports.URI = "mongodb://localhost:27017/UrWay";
+
+
+// module.exports = 
+// {
+//     "URI": "mongodb+srv://mani:x638mrFfD3v161cs@mongodbserver.wpmmb.mongodb.net/Covid-19_Vaccine_Survey?retryWrites=true&w=majority"
+// }
